f068d6d1049879e80ead329c4cd7774ad1d7ba4b
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2023-11-15 15:18:21 +0000

    03 collating data updated to create first dataset for attempting to perform modelling

--- R/03_collating_data.R
+++ R/03_collating_data.R
@@ -2,14 +2,28 @@ source("R/00_libraries.R")
 source("R/01_utils.R")
 
 dc_data <- list.files("data", full.names = TRUE) |> 
+  (\(x) x[!grepl("configuration-table|modelling_data", x)])() |> 
   purrr::map_dfr(
     read.csv
   ) |> 
+  filter(
+    grepl("annual", frequency),
+    grepl("^Q", org)
+  ) |> 
   select(
     "metric",
-    "year", "quarter", "month", "frequency",
-    "org", "org_name", "at_code",
-    "numerator", "denominator", "value"
+    "year", 
+    "org", 
+    "value"
+  ) |> 
+  pivot_wider(
+    names_from = metric,
+    values_from = value
   )
 
 
+write.csv(
+  dc_data,
+  "data/modelling_data.csv",
+  row.names = FALSE
+)
